aab5c1a4dc0be827d83505b50f546e50dcf3b3fc
Rodrigo Moraes Silveira <rodrigoms2004@gmail.com>
2019-11-23 11:14:03 -0300

    sobrecarga de operadores

--- cap07_ClassesObjetos/DataStructClassCpp/data.h
+++ cap07_ClassesObjetos/DataStructClassCpp/data.h
@@ -31,17 +31,96 @@ public:
   // }
   Data() = default;
 
-  Data(short dia, short mes, short ano);
 
-  void validar();
+  inline Data(short dia, short mes, short ano) {
+    alterar(dia, mes, ano);
+  }
+
+  inline void validar()
+  {
+    m_ok = m_dia >= 1      && m_dia <= ultimoDiaMes() &&
+          m_mes >= 1       && m_mes <= 12             &&
+          m_ano >= ANO_MIN && m_ano <= ANO_MAX;
+  }
+
+
+  inline short ultimoDiaMes() const
+  {
+    if(m_mes == FEVEREIRO && anoBissexto()) {
+      return 29;
+    } else if (m_mes == FEVEREIRO && !anoBissexto()) {
+      return 28;
+    } else if ( (m_mes & 1) && m_mes <= 7) {  // meses impares, janeiro a julho
+      return 31;
+    } else if ( !(m_mes & 1) && m_mes <=7) {  // meses pares, janeiro a julho
+      return 30;
+    } else if ( (m_mes & 1) && m_mes > 7) {   // meses impares, agosto a dezembro
+      return 30;
+    } else {                                  // messes pares, agosto a dezembro
+      return 31;
+    }
+    return (m_mes == FEVEREIRO) ? 28 + anoBissexto() : // se for bissexto soma 28 + 1 = 29, senão 28 + 0 = 28
+              30 + (
+                (m_mes & 1) // se m_mes, impar = 1, par = 0
+                ^ (m_mes > JULHO)); // se > JULHO, resulta 0, senão resuta em 1
+                                    // ^ é o operador XOR bitwise    
+                                    // mês par ente jan e jul = 0^0 + 30 = 30 dias
+                                    // mês impar ente jan e jul = 1^0 + 30 = 31 dias
+
+                                    // mês par ente ago e dez = 0^1 + 30 = 31 dias
+                                    // mês impar ente ago e dez = 1^1 + 30 = 30 dias
+
+  /*
+    1 & 1 => 001 & 001 => 001
+    2 & 1 => 010 & 001 => 000
+    3 & 1 => 011 & 001 => 001
+    4 & 1 => 100 & 001 => 000
+
+    impar & 1 = 1
+    par & 1 = 0
+  */
+
+
+    // return (m_mes == FEVEREIRO) ? 28 + anoBissexto() :
+    //                                   30 + ((m_mes & 1) ^ (m_mes > JULHO));
+  }
+
+  inline bool anoBissexto() const 
+  {
+    // return ((m_ano % 4 == 0) && (m_ano % 100 != 0)) || (m_ano % 400 == 0);
+    // return !(m_ano % 4) && ( (m_ano % 100) || !(m_ano % 400) );
+    // return !(m_ano & 3) && ( (m_ano % 100) || !(m_ano % 400) );
+    return (!(m_ano & 3) && (m_ano % 100)) || !(m_ano % 400);
+  }
 
   void alterar(short dia, short mes, short ano);
 
-  short ultimoDiaMes() const;
-
   void imprimir () const;
 
-  bool anoBissexto() const;
+  int comparar(const Data &other) const;
+
+  // sobrecarga de operadores
+  
+  inline bool operator<(const Data &other) const
+  {
+    return comparar(other) < 0;
+  }
+  
+  inline bool operator>(const Data &other) const
+  {
+    return comparar(other) > 0;
+  }
+  
+  inline bool operator==(const Data &other) const
+  {
+    return comparar(other) == 0;
+  }
+  
+  // inline bool operator!=(const Data &other) const
+  // {
+  //   return comparar(other) != 0;
+  // }
+
 
 };
 
--- cap07_ClassesObjetos/DataStructClassCpp/main.cpp
+++ cap07_ClassesObjetos/DataStructClassCpp/main.cpp
@@ -1,9 +1,12 @@
 #include "data.h"
 #include <iostream>
+#include <list>
 
 // g++ -o main.o  main.cpp data.h data.cpp -std=c++11
 
 using namespace std;
+
+void testarDatas (const Data&, const Data&);
 
 int main() {
 
@@ -35,5 +38,86 @@ int main() {
   d1.alterar(31,9,2001) ; d1.imprimir(); // resultado: ERRO | Setembro tem apenas 30 dias
   d1.alterar(31,12,2001); d1.imprimir(); // resultado: 31/12/2001
 
+
+  Data pagamento(23, 11, 2019), vencimento1(23, 11, 2019)
+                              , vencimento2(23, 11, 2018)  
+                              , vencimento3(23, 11, 2020)
+                              , vencimento4(23, 11, 2019)  
+                              , vencimento5(23, 10, 2018)
+                              , vencimento6(23, 12, 2020)
+                              , vencimento7(23, 11, 2019)
+                              , vencimento8(22, 11, 2018)  
+                              , vencimento9(24, 11, 2020);  
+
+
+  list<Data> listaDatasVencimento;
+
+  // validando ano 
+  listaDatasVencimento.push_back(vencimento1);
+  listaDatasVencimento.push_back(vencimento2);
+  listaDatasVencimento.push_back(vencimento3);
+
+  // validando mes
+  listaDatasVencimento.push_back(vencimento4);
+  listaDatasVencimento.push_back(vencimento5);
+  listaDatasVencimento.push_back(vencimento6);
+
+  // validando dia
+  listaDatasVencimento.push_back(vencimento7);
+  listaDatasVencimento.push_back(vencimento8);
+  listaDatasVencimento.push_back(vencimento9);
+
+  
+  for (list<Data>::iterator it = listaDatasVencimento.begin(); it != listaDatasVencimento.end(); it++) {
+    int currentIndex = distance(listaDatasVencimento.begin(), it);
+    // cout << currentIndex << endl;
+
+    if (currentIndex == 0) {
+      cout << "\nValidando anos" << endl;
+    }
+
+    if (currentIndex == 3) {
+      cout << "\nValidando meses" << endl;
+    }
+
+    if (currentIndex == 6) {
+      cout << "\nValidando dias" << endl;
+    }
+
+    testarDatas(pagamento, *it);
+    // it->imprimir();
+  }
+
   return 0;
+}
+
+void testarDatas (const Data &pagamento, const Data &vencimento) {
+
+  // usando sobrecarga de operadores
+  if(pagamento > vencimento)
+  {
+    cout << "Pagamento adiantado!\n";
+  }
+  else if(pagamento == vencimento)
+  {
+    cout << "No prazo!\n";
+  }
+  else if(pagamento < vencimento)
+  {
+    cout << "Atrasado!\n";
+  }
+
+
+  // if(pagamento.comparar(vencimento) > 0)
+  // {
+  //   cout << "Pagamento adiantado!\n";
+  // }
+  // else if(pagamento.comparar(vencimento) == 0)
+  // {
+  //   cout << "No prazo!\n";
+  // }
+  // else if(pagamento.comparar(vencimento) < 0)
+  // {
+  //   cout << "Atrasado!\n";
+  // }
 }
